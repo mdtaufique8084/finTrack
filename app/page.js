5f9bc89a6d49600402b521fb95d5ee5ce3112a99
Md Taufique <mdtaufique8084@gmail.com>
2025-05-20 02:31:11 +0530

    Landing-Page : added

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <div>
-    
-  </div>
+  return (
+    <div>
+      <HeroSection />
+    </div>);
 }
