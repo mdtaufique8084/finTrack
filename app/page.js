f1985f72b5e8f1c7ab67089fc8390d1a47725960
Md Taufique <mdtaufique8084@gmail.com>
2025-05-24 20:18:34 +0530

    landing page : added

--- app/page.js
+++
@@ -1,9 +0,0 @@
-import HeroSection from "@/components/hero";
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <div>
-      <HeroSection />
-    </div>);
-}
--- components/hero.jsx
+++ components/hero.jsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
+          <Link href="https://github.com/mdtaufique8084">
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
